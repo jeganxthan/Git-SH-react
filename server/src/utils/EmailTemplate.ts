5a9c6b00ab0545eca341e5c8a7343b694e0f81c4
jeganathan I <jegan4044@gmail.com>
2025-09-07 15:08:24 +0530

    Update EmailTemplate.ts

--- server/src/utils/EmailTemplate.ts
+++ server/src/utils/EmailTemplate.ts
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Static Template</title>
-
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
-      rel="stylesheet"
-    />
-    <script src="https://cdn.tailwindcss.com"></script>
-  </head>
+export const otpTemplate = (name: string, otp: string) => `
   <body
     style="
       margin: 0;
@@ -132,4 +119,4 @@
       </footer>
     </div>
   </body>
-</html>
+`;
